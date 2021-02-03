dec75a491446018edc73272b074c196feaa8e9bd
Papoutsoglou <evangelia.papoutsoglou@wur.nl>
2021-02-03 10:16:20 +0100

    Add first draft

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-# FAIR_CxE
+# FAIR_CxE: CxE potato data integration
 
 This repository holds the files associated with Chapter 4 of E. Papoutsoglou's thesis. 
+
+You can find 3 docker containers for the following:
+
+1. A simple FAIR data point for the dataset in question (uses Python 3 / flask)
+2. A SPARQL server hosting the phenotypic (meta)data and the weather datasets (uses Fuseki)
+3. Jupyter notebooks for:
+	* the conversion of a MIAPPE spreadsheet file to RDF (phenotypic metadata)
+	* the conversion of tabular phenotypic data files to RDF
+	* the exploration of the produced RDF as pulled from container (2), and some visualizations.
---
+++ all_containers/server-fdp/fdp.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Not implemented.'
+    
+@app.route('/FDP')
+def fdp():
+    return render_template('./fdp.ttl.html')
+    
+    
+@app.route('/FDP/catalog/phenotypic.ttl')
+def catalog():
+    return render_template('./catalog.ttl.html')
+    
+    
+@app.route('/FDP/dataset/Dataset_1.ttl')
+def dataset():
+    return render_template('./dataset.ttl.html')
+    
+    
+@app.route('/FDP/distribution/Pheno_dataset_1_sparql.ttl')
+def distribution():
+    return render_template('./distribution.ttl.html')
+    
+    
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3131)
